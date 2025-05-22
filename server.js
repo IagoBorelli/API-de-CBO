686d9b327ac4c2bc74f2d42e6ab6ed0a71df9d0d
Iago Borelli <110144568+IagoBorelli@users.noreply.github.com>
2025-05-22 16:30:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+const authRoutes = require('./routes/authRoutes');
 const cboRoutes = require('./routes/cboRoutes');
+
+app.use('/auth', authRoutes);
 
 app.use(express.json());
 app.use('/cbos', cboRoutes);
